ee8d7f0fd8bfe44405240e548887bc2c39fc9741
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-29 00:19:39 +0300

    fix telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,19 @@ return {
   tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    local telescope = require("telescope")
     local builtin = require("telescope.builtin")
+
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
+      },
+    })
+
+    telescope.load_extension("ui-select")
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<C-p>',
       function()
