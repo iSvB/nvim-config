9631322a77b19c878c8b7cbf4eb0de4445faa01d
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-28 23:08:10 +0300

    use lspattach

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -6,16 +6,20 @@ return {
     vim.lsp.enable('ts_ls')
     vim.lsp.enable('gopls')
 
-    vim.lsp.config('*', {
-      vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, {}),
-      vim.keymap.set('n', '<leader>ic', vim.lsp.buf.incoming_calls, {}),
-      vim.keymap.set('n', '<leader>oc', vim.lsp.buf.outgoing_calls, {}),
-      vim.keymap.set('n', '<leader>re', vim.lsp.buf.references, {}),
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {}),
-      vim.keymap.set('n', '<leader>de', vim.lsp.buf.declaration, {}),
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}),
-      vim.keymap.set('n', '<leader>td', vim.lsp.buf.type_definition, {}),
-      vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, {}),
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(event)
+        local opts = { buffer = event.buf }
+
+        vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>ic', vim.lsp.buf.incoming_calls, opts)
+        vim.keymap.set('n', '<leader>oc', vim.lsp.buf.outgoing_calls, opts)
+        vim.keymap.set('n', '<leader>re', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>de', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<leader>td', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, opts)
+      end,
     })
   end
 }
