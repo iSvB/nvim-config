f8a8d7dccb1b6596165a08ab8670dce57baa8cfa
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-28 23:22:54 +0300

    remove deprecated call

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,18 @@
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 
 vim.keymap.set("n", "<leader>nh", function()
-  Snacks.notifier.show_history()
+  require("snacks").notifier.show_history()
 end, { desc = "Show notification history" })
 
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+-- Diagnostics
+
+vim.keymap.set("n", "[d", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Previous diagnostic" })
+
+vim.keymap.set("n", "]d", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Next diagnostic" })
+
+vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
