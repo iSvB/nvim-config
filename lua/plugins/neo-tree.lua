2376221d0c93c3d0b05d5aedf1bb7a061f1b5ee0
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-29 00:22:48 +0300

    upd neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,8 +11,13 @@ return {
   ---@module "neo-tree"
   ---@type neotree.Config?
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<C-b>', ':Neotree buffers reveal float<CR>', {})
+    local function map(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, { desc = desc })
+    end
+
+    map('<C-n>', '<cmd>Neotree filesystem reveal left<CR>', 'Reveal file tree')
+    map('<C-b>', '<cmd>Neotree buffers reveal float<CR>', 'Show buffers tree')
+
     require("neo-tree").setup({
     })
   end,
