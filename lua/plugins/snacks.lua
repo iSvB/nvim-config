5c3f2dd0a59d9f3cfafd0ba583fa94f728587e3a
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-29 00:45:12 +0300

    upd snacks.lua

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,9 +4,6 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     explorer = { enabled = true },
@@ -15,7 +12,7 @@ return {
     picker = { enabled = true },
     notifier = {
       enabled = true,
-      timeout = 10000
+      timeout = 10000,
     },
     quickfile = { enabled = true },
     scope = { enabled = true },
