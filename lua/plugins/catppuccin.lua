3076cb159ae49aff421d432eb29842aaf0505b89
Ivan Burdukov <i.burdukov@gmail.com>
2026-04-29 00:33:36 +0300

    upd catppuccin.lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,7 +2,11 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+  opts = {
+    flavour = "mocha",
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
