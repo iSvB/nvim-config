0d2be08e788a00b2d4aec33f4fec30cfc2eb835c
Ivan Burdukov <i.burdukov@gmail.com>
2026-06-26 01:24:33 +0300

    upd treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "javascript",
-      "c",
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-      "c_sharp",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+  config = function()
+    require("nvim-treesitter").setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua",
+        "javascript",
+        "c",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "cs",
+        "markdown",
+      },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
